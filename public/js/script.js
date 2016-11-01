abf71fd678998366ba2695f702eb73b58630b8df
sherri010 <aminshahidi.sh@gmail.com>
2016-11-01 13:00:33 -0700

    adds nickname to chat

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,22 @@
+
+  $(document).ready(function(){
   var socket = io();
+
+  var name = prompt(" You are?");
+  while(name =="" || name == null){
+    name = prompt(" I said you are?");
+  }
+  $('#username').html("<b> logged in as </b> =>  "+name);
   $('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
+
+    //var name = $('#username').val();
+    finalmsg = name+": "+ $('#m').val()
+
+  socket.emit('chat message',finalmsg);
     $('#m').val('');
     return false;
   });
   socket.on('chat message',function(msg){
        $('#messages').append($('<li>').text(msg));
      });
+});
