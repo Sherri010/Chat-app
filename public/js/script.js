d3265f6b7c8efa2063a0de9dec35ecedbd9261fc
sherri010 <aminshahidi.sh@gmail.com>
2016-11-01 21:28:43 -0700

    user can type live

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,40 @@
 
   $(document).ready(function(){
-  var socket = io();
+    var socket = io();
 
-  var name = prompt(" You are?");
-  while(name =="" || name == null){
-    name = prompt(" I said you are?");
-  }
-  $('#username').html("<b> logged in as </b> =>  "+name);
-  $('form').submit(function(){
+    //makes sure to get the username...for now :)
+    var name = prompt(" You are?");
+    while(name =="" || name == null){
+      name = prompt(" I said you are?");
+    }
 
-    //var name = $('#username').val();
-    finalmsg = name+": "+ $('#m').val()
+    $('#username').html("<b> logged in as </b> =>  "+name);
 
-  socket.emit('chat message',finalmsg);
-    $('#m').val('');
-    return false;
-  });
-  socket.on('chat message',function(msg){
-       $('#messages').append($('<li>').text(msg));
-     });
+
+     var status="free";
+    $('#m').keypress(function() {
+    socket.emit('chat message',{user:name,msg:$('#m').val()});
+    status="typing";
+    });
+    //user writes a msg
+    $('form').submit(function(){
+      //finalmsg = name+": "+ $('#m').val()
+      socket.emit('chat message',{user:name,msg:$('#m').val()});
+      $('#m').val('');
+        return false;
+    });
+
+    //recieves a msg
+    socket.on('chat message',function(msg){
+         // cleanUp();
+         // var newMsg = $('li').attr('class',msg);
+         // newMsg.text(msg);
+          console.log(msg.user," ",msg.msg);
+         $('#messages').html("<li>"+msg.msg+"</li>");
+       });
+
+    // function cleanUp(){
+    //   var last =$('#messages').last();
+    //   console.log(last)
+    // }
 });
